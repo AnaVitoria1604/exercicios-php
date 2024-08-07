dc24eb245cff07e4168420b05e9bea273e85df68
AnaVitoria1604 <ana.lourenco.senai404@gmail.com>
2024-08-07 16:45:08 -0300

    codigo php dia 07/08

---
+++ ifelse.php
@@ -0,0 +1,27 @@
+<?php
+
+$nota1 = 7;
+$nota2 = 6;
+$nota3 = 8;
+
+echo "A 1° nota é: $nota1 <br/>";
+if ($nota1 >= 7) {
+    echo "Aprovado! <br/>";
+}else{
+    echo "Reprovado! <br/>";
+}
+echo "A 2° nota é: $nota2 <br/>";
+if ($nota2 >= 7) {
+    echo "Aprovado! <br/>";
+}else{
+    echo "Reprovado! <br/>";
+}
+echo "A 3° nota é: $nota3 <br/>";
+
+if ($nota3 >= 7) {
+    echo "Aprovado! <br/>";
+}else{
+    echo "Reprovado! <br/>";
+}
+
+?>
