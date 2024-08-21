18a4c2f76e1ef785e64c7624729f18a0068d1784
AnaVitoria1604 <ana.lourenco.senai404@gmail.com>
2024-08-21 15:34:04 -0300

    Add files via upload

---
+++ while.exercicio.php
@@ -0,0 +1,18 @@
+<?php
+$i = 30;
+$contador = 1;
+
+if ($i>0){
+    while ($contador <= $1 ){
+        if ($contador % 2 ==0)
+        echo "O contador par é: $contador<br>";
+    $contador++;
+    }
+}
+
+else{
+    echo "Não é par";
+}
+
+
+?>
